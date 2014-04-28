9daee3124db7ebe18e56df5f8b52d534949aeb1a
cdmihai <codobanmihai@gmail.com>
2014-04-28 00:54:08 -0700

    Added getters, setters and comments

--- GroupedBarChart.js
+++ GroupedBarChart.js
@@ -1,30 +1,45 @@
+/*
+Based on: http://bl.ocks.org/mbostock/3887051
+*/
+
+/*
+Displays a grouped bar chart.
+The chart takes as input the name of the first column (via mainGroupName()).
+This is the column around which the rest of the columns are grouped by.
+*/
 function groupedBarChart() {
 
+	//pading around the chart
 	var margin = {
 			top: 20,
 			right: 20,
 			bottom: 30,
 			left: 40
-		},
-		width = 960 - margin.left - margin.right,
-		height = 500 - margin.top - margin.bottom;
+		};
+	var width = 960 - margin.left - margin.right;
+	var height = 500 - margin.top - margin.bottom;
+	var yLabel ="";
 
 	//column name of first grouping
+	//the bars will be grouped by this column
 	var mainGroupingName = ""
 
+	//scale responsible for the main grouping
 	var x0 = d3.scale.ordinal()
-		.rangeRoundBands([0, width], .1);
 
+	//scale responsible for the grouped bars
+	//it "exists" inside x0: its range is as big as one band in X0
 	var x1 = d3.scale.ordinal();
 
 	var y = d3.scale.linear()
-		.range([height, 0]);
 
+	//a color scale for the grouped bars
 	var color = d3.scale.ordinal()
-		//.range(["#98abc5", "#8a89a6", "#7b6888", "#6b486b", "#a05d56", "#d0743c", "#ff8c00"]);
 
+	//generate as many colors as grouped bars
 	var constructColor = function(nrValues){
 
+		//some magic tweaking of colors to ensure non blinding colors
 		var max = parseInt("9F042F", 16);
 		var min = parseInt("102030", 16);
 
@@ -39,7 +54,7 @@ function groupedBarChart() {
 		color.range(colors);
 	}
 
-
+	
 	var xAxis = d3.svg.axis()
 		.scale(x0)
 		.orient("bottom");
@@ -49,6 +64,7 @@ function groupedBarChart() {
 		.orient("left")
 		.tickFormat(d3.format(".2s"));
 
+	//add styling to the axis. 
 	var styleAxis = function (element) {
 		element.style("fill", "none")
 			.style("stroke", "#000")
@@ -62,13 +78,13 @@ function groupedBarChart() {
 
 		selection.each(function (data) {
 
+			//retrieve the inner grouping column names as all the column names of a node except the primary one
 			var secondGroupingNames = d3.keys(data[0]).filter(function (key) {
 				return key !== mainGroupingName;
 			});
 
-			color.domain(secondGroupingNames);
-			constructColor(secondGroupingNames.length);
-
+			//add a new field to all data objects that contains an array off all the values in a key - value format
+			//this is needed to be able to join the grouped data to its marks
 			data.forEach(function (d) {
 				d.secondGrouping = secondGroupingNames.map(function (name) {
 					return {
@@ -77,13 +93,24 @@ function groupedBarChart() {
 					};
 				});
 			});
+			
+			color.domain(secondGroupingNames); //the secondary, grouped columns need unique colors
+			constructColor(secondGroupingNames.length); //generate as many colors as secondoray grouped columns are
 
+			x0.rangeRoundBands([0, width], .1); 
+
+			//the domain of the primary grouping
 			x0.domain(data.map(function (d) {
 				return d[mainGroupingName];
 			}));
 
+			//the domain of the secondary grouping represents the values of the secondary grouped columns
+			//the range is the range of one of the primary groupings. This ensures that the grouped columns fit in the larger grouping
 			x1.domain(secondGroupingNames).rangeRoundBands([0, x0.rangeBand()]);
 			
+			y.range([height, 0]);
+
+			//compute the maximum of all the nested values
 			y.domain([0, d3.max(data, function (d) {
 				return d3.max(d.secondGrouping, function (d) {
 					return d.value;
@@ -97,11 +124,13 @@ function groupedBarChart() {
 				.append("g")
 				.attr("transform", "translate(" + margin.left + "," + margin.top + ")"); //translate to margin corner
 
+			//Ox axis styling
 			svg.append("g")
 				.call(styleAxis)
 				.attr("transform", "translate(0," + height + ")")
 				.call(xAxis);
 
+			//Oy axis styling and label
 			svg.append("g")
 				.call(styleAxis)
 				.call(yAxis)
@@ -110,41 +139,45 @@ function groupedBarChart() {
 				.attr("y", 6)
 				.attr("dy", ".71em")
 				.style("text-anchor", "end")
-				.text("Population");
+				.text(yLabel);
 
+			//create groups for each main grouping value
 			var mainGrouping = svg.selectAll(".mainGrouping")
-				.data(data)
+				.data(data) //the top level data is joined, each row in the data gets one group
 				.enter().append("g")
 				.attr("class", "g")
 				.attr("transform", function (d) {
-					return "translate(" + x0(d[mainGroupingName]) + ",0)";
+					return "translate(" + x0(d[mainGroupingName]) + ",0)"; //each main grouping is translated to a position via the x0 scale
+
 				});
 
+			//create the grouped bars
 			mainGrouping.selectAll("rect")
-				.data(function (d) {
+				.data(function (d) { //for each row, join its nested data to rectangles from inside each higher level group
 					return d.secondGrouping;
 				})
 				.enter().append("rect")
-				.attr("width", x1.rangeBand())
+				.attr("width", x1.rangeBand()) //use x1 to get the height of a single bar, so it fits inside the larger band defined by x0
 				.attr("x", function (d) {
-					return x1(d.name);
+					return x1(d.name); //use x1 to obtain the position of a specific bar
 				})
 				.attr("y", function (d) {
-					return y(d.value);
+					return y(d.value); //use y to scale the actual value
 				})
 				.attr("height", function (d) {
 					return height - y(d.value);
 				})
 				.style("fill", function (d) {
-					return color(d.name);
+					return color(d.name); //get the color for a specific bar
 				});
 
+
 			var legend = svg.selectAll(".legend")
-				.data(secondGroupingNames.slice().reverse())
+				.data(secondGroupingNames.slice())
 				.enter().append("g")
 				.attr("class", "legend")
 				.attr("transform", function (d, i) {
-					return "translate(0," + i * 20 + ")";
+					return "translate(0," + i * 20 + ")"; //put the legend boxes vertically on top of each other
 				});
 
 			legend.append("rect")
@@ -165,9 +198,39 @@ function groupedBarChart() {
 
 	}
 
+	//here be getters and setters
+
 	chart.mainGroupingName = function (value) {
 		if (!arguments.length) return dateFormat;
 		mainGroupingName = value;
+
+		return this;
+	};
+
+	chart.width = function (value) {
+		if (!arguments.length) return width;
+		width = value - margin.left - margin.right;
+
+		return this;
+	};
+
+	chart.height = function (value) {
+		if (!arguments.length) return height;
+		height = value - margin.top - margin.bottom;
+
+		return this;
+	};
+
+	chart.yLabel = function (value) {
+		if (!arguments.length) return yLabel;
+		yLabel = value;
+
+		return this;
+	};
+
+	chart.yLabel = function (value) {
+		if (!arguments.length) return yLabel;
+		yLabel = value;
 
 		return this;
 	};
